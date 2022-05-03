ac6cf09820c4f54037cdca30e3ef51ffa876ea62
Bastiloko <bastianandresxb@gmail.com>
2022-05-03 11:51:14 -0400

    contract kit and exponential number handling

--- utils/web3Utils.js
+++ utils/web3Utils.js
@@ -1,4 +1,6 @@
-const fiveYearsInSeconds = 31536000 * 5;
+const fromExponential = require("from-exponential");
+
+const oneYearInSeconds = 31536000;
 
 // Count decimals of a given number
 const countDecimals = (number) => {
@@ -7,8 +9,12 @@ const countDecimals = (number) => {
   let str = number.toString();
   //console.log("string: ", str);
 
+  if (str.includes("e")) {
+    str = fromExponential(str);
+  }
+
   const splitted = str.split(".");
-  //console.log(`decimals: 0.${splitted[1]}`);
+  console.log(`decimals: 0.${splitted[1]}`);
   return splitted[1].length;
 };
 
@@ -19,7 +25,7 @@ const extractNumberTypes = (object) => {
 
   let numbers = [];
   keys.forEach((key) => {
-    //console.log(`key ${key}: ${object[key]} - type: ${typeof object[key]}`);
+    console.log(`key ${key}: ${object[key]} - type: ${typeof object[key]}`);
     if (typeof object[key] === "number") {
       //console.log("number found: ", object[key]);
       numbers.push(object[key]);
@@ -32,9 +38,17 @@ const extractNumberTypes = (object) => {
 const extractMaxDecimals = (numbers) => {
   let maxDecimals = 0;
   numbers.forEach((number) => {
+    if (number.toString().includes("e")) {
+      number = fromExponential(number);
+    }
     let currentDecimals = countDecimals(number);
     if (currentDecimals > maxDecimals) maxDecimals = currentDecimals;
   });
+
+  console.log("max decimals: ", maxDecimals);
+  if (maxDecimals > 10) {
+    maxDecimals = 10;
+  }
 
   return maxDecimals;
 };
@@ -42,18 +56,19 @@ const extractMaxDecimals = (numbers) => {
 // Converts an array of objects to an array of arrays,
 //  as solidity handles arrays of structs this way!
 // ej: [{a, b, c}], {d, e, f}] -> [[a, b, c], [d, e, f]]
-const convertSpeciesToArray = (species) => {
+const convertSpeciesToArray = (species, landSize) => {
   let speciesAsArrays = [];
+  console.log("species from convert: ", species);
 
   species.map((specie) => {
     //console.log("before refactor: ", specie);
-    specie = refactorSpecie(specie);
+    specie = refactorSpecie(specie, landSize);
     //console.log("refactored...: ", specie);
     let {
       speciesAlias,
       scientificName,
+      initialTCO2perYear,
       size,
-      initialTCO2,
       TCO2perSecond,
       density,
     } = specie;
@@ -69,7 +84,7 @@ const convertSpeciesToArray = (species) => {
       normalizeNumber(size, decimals), // As solidity only handle numbers as integers!
       decimals,
       normalizeNumber(TCO2perSecond, decimals),
-      normalizeNumber(initialTCO2, decimals),
+      normalizeNumber(initialTCO2perYear, decimals),
       0,
       0,
       0
@@ -118,12 +133,13 @@ const refactorPoints = (points) => {
 };
 
 // Convert numbers from strings to numbers
-const refactorSpecie = (specie) => {
-  specie.initialTCO2 = Number(getInitialTCO2([specie]));
-  specie.size = Number(specie.size);
+const refactorSpecie = (specie, landSize) => {
+  specie.initialTCO2perYear = Number(Math.round(specie.initialTCO2perYear));
+  specie.size = Number(landSize / specie.density);
   specie.density = Number(specie.density);
   specie.TCO2perSecond = Number(specie.TCO2perSecond);
 
+  console.log("specie from refactor: ", specie);
   return specie;
 };
 
@@ -140,20 +156,55 @@ const maxDecimalsOf = (object) => {
 
 // This function normalize a number to account for decimals
 // given that solidity only work with integer numbers
-const normalizeNumber = (number, decimals = 0) => {
-  return Math.round(number * Math.pow(10, decimals));
+const normalizeNumber = (number, decimals = 0, round = true) => {
+  // number = Number(number);
+  // decimals = Number(decimals);
+
+  let result = number * Math.pow(10, decimals);
+  //console.log(result);
+
+  result = result.toString();
+
+  if (result.includes("e")) {
+    result = fromExponential(result);
+  }
+
+  if (round) return Math.round(result);
+
+  decimals = decimals.toString();
+  let rounded = roundValue(result, decimals * -1).toString();
+
+  if (rounded.includes("e")) {
+    rounded = fromExponential(rounded);
+  }
+  return rounded;
+};
+
+const roundValue = (value, decimals) => {
+  value = value.toString();
+  console.log("decimals: ", decimals);
+  if (value.includes("e")) {
+    console.log("value includes e: ", value);
+    value = fromExponential(value);
+    console.log("after from exponential: ", value);
+  }
+
+  const result = Number(Math.round(value + "e+" + decimals) + "e-" + decimals);
+  console.log("result: ", result);
+
+  return result;
 };
 
 // Get the CO2 that a given specie offsets in five years
-const getInitialTCO2 = (species) => {
-  let initialTCO2 = 0;
+const getInitialTCO2perYear = (species) => {
+  let initialTCO2perYear = 0;
 
   species.map((specie) => {
-    initialTCO2 += specie.TCO2perSecond * fiveYearsInSeconds;
-    //console.log("initial tco2 now: ", initialTCO2);
+    initialTCO2perYear += Number(specie.initialTCO2perYear);
+    console.log("initial tco2 now: ", initialTCO2perYear);
   });
 
-  return Math.round(initialTCO2);
+  return Math.round(initialTCO2perYear);
 };
 
 module.exports = {
@@ -161,5 +212,7 @@ module.exports = {
   normalizeNumber,
   convertSpeciesToArray,
   convertPointsToArray,
-  getInitialTCO2,
+  getInitialTCO2perYear,
+  oneYearInSeconds,
+  roundValue,
 };
